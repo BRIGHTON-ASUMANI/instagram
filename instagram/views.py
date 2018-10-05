017d811456b53b7afbe9ff06930e2abc82cb074f
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-10-06 02:00:23 +0300

    ensured that @login required is imported

--- instagram/views.py
+++ instagram/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required.
 
 # Create your views here.
-
+@login_required(login_url='/accounts/login/')
 def auth(request):
     return render(request, 'auth/register.html', {})
-
-def login_user(request):
-    return render(request, 'auth/login.html', {})
