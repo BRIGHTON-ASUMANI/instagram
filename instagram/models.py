ad24ef10fa55ffa98368e95dc603e9173634ed55
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-10-06 10:28:36 +0300

    added the post model

--- instagram/models.py
+++ instagram/models.py
@@ -1,4 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
+import datetime as dt
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=60)
+    post = models.TextField()
+    posted_by = models.ForeignKey(User,on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    post_image = models.ImageField(upload_to = 'posts/')
